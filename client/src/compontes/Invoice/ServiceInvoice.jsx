5c5dda4174f552773162994d46d61bacb6472f79
sarathcredot <sarath@credot.in>
2025-07-28 15:29:22 +0530

    url changed

--- client/src/compontes/Invoice/ServiceInvoice.jsx
+++ client/src/compontes/Invoice/ServiceInvoice.jsx
@@ -242,7 +242,7 @@ const EditableInvoice = () => {
       formData.append("finalData", JSON.stringify(finalData));
       formData.append("pdf", pdfBlob, `invoice-${invoiceData.invoiceNumber}.pdf`);
 
-      const result = await axios.post("https://server-api-breezy.onrender.com/api/invoice/serviceinvoice", formData)
+      const result = await axios.post("https://breezy-invoice-app.onrender.com/api/invoice/serviceinvoice", formData)
       // const result = await axios.post("http://localhost:3018/api/invoice/serviceinvoice", formData)
 
       toast.success("invoice sent to whatsapp !!")
